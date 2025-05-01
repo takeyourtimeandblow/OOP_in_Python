65d447c7cd0aa66daaa0071931a6750efeef504f
Groovie <104006670+takeyourtimeandblow@users.noreply.github.com>
2025-05-01 22:52:55 +0700

    Update FunctionConstructor.py

--- FunctionConstructor.py
+++ FunctionConstructor.py
@@ -3,14 +3,14 @@ import sys
 
 class FunctionConstructor:
     def __init__(self):
-        self.funcs: dict[str : str] = {}
+        self.funcs: dict[str : str] = {} # name of function is key, function will be formated to string
 
     def define(self, name, func):
         for i in range(len(func)):
             if func[i] in self.funcs:
-                func[i] = '(' + self.funcs[func[i]] + ')'
-        self.funcs[name] = ' '.join(func)
-        yield True
+                func[i] = '(' + self.funcs[func[i]] + ')' # brackets to save priority of actions
+        self.funcs[name] = ' '.join(func) # important to save space between all elements of function
+        yield True # necessary, but 26's line will not work
 
     def calculate(self, name, args):
         for i in args:
@@ -24,7 +24,11 @@ def main():
     for txt in sys.stdin:
         txt = txt.split()
         x = [i for i in eval(f'f.{txt[0]}("{txt[1]}", {txt[2:]})')]
-        print(x) if True not in x else None
+        """if txt would be equal 'define f1 x * x'
+        then upper argument to function eval should be equal:
+        f.define("f1", ['x', '*', 'x'])
+        """
+        print(x) if True not in x else None # just ignore define call
 
     print(f.funcs)
 
